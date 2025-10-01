1fe3d865ab42fdbcb958c5b38fe3a0adfa4a3853
Dylan Ngu <dylan.ngu11@gmail.com>
2025-10-01 19:08:15 -0400

    Module 5 extra - Static HTML to templates with JSON data

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var mealData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 /* GET Meals view */
 const meals = (req, res) => {
-    res.render('meals', { Meals: true, title: 'Travlr Getaways'});
+    res.render('meals', { Meals: true, title: 'Travlr Getaways', mealData});
 };
 
 module.exports = {
--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var newsData = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+
 /* GET News view */
 const news = (req, res) => {
-    res.render('news', { News: true, title: 'Travlr Getaways'});
+    res.render('news', { News: true, title: 'Travlr Getaways', newsData});
 };
 
 module.exports = {
